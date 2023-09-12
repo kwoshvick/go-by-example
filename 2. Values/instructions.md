1a5711d3ff8e9f4cd77c25dcbcb5033f48fd7d3d
kwoshvick <kwoshvick@gmail.com>
2023-09-12 09:34:55 +0300

    values

---
+++ 2. Values/instructions.md
@@ -0,0 +1,5 @@
+Go has various value types including strings, integers, floats, booleans, etc. 
+
+Strings, which can be added together with +.
+Integers and floats.
+Booleans, with boolean operators as you’d expect.
---
+++ 2. Values/values.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	//string
+	fmt.Println("go" + "lang")
+
+	//int
+	fmt.Println("1+1=", 1+1)
+
+	//float
+	fmt.Println("7.0/3.0", 7.0/3.0)
+
+	//boolean
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
+}
